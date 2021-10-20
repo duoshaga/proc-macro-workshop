793fa74ebf7f7d395bc67decf870ae94debbedc5
mayue <mayuetmy@gmail.com>
2021-10-20 10:27:59 +0800

    test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,24 @@
+#![allow(unused)]
 use proc_macro::TokenStream;
+use quote;
+use syn::{parse_macro_input, AttributeArgs, DeriveInput, Item, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
-
-    unimplemented!()
+    let aa = parse_macro_input!(input as DeriveInput);
+    println!("{:#?}\naaa", aa);
+    TokenStream::new()
+    // let a = quote::quote! {
+    // //    println!("aaaaaa");
+    // };
+    // a.into()
+    // input
+    // unimplemented!()
+}
+#[proc_macro_attribute]
+pub fn proc_attr_test(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("{:#?}", parse_macro_input!(attr as AttributeArgs));
+    println!("{:#?}", parse_macro_input!(item as Item));
+    TokenStream::new()
 }
